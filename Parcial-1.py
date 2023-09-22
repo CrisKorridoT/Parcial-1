5f7abaa0cb400dcf8b9d7ae43aac510c394429ba
Cristian Arias <130936747+CrisKorridoT@users.noreply.github.com>
2023-09-22 08:23:39 -0500

    Create Parcial-1.py
    Conjetura de Collatz con el promedio de 3 valores menores que 259

---
+++ Parcial-1.py
@@ -0,0 +1,40 @@
+n1 = None
+n2 = None
+n3 = None
+
+while n1 is None:
+    valor = int(input("Ingrese el primer número (mayor que 0 y menor que 259): "))
+    if valor < 0 or valor > 259:
+        print("Error: el valor ingresado no cumple con las condiciones.")
+    else:
+        n1 = valor
+while n2 is None:
+    valor = int(input("Ingrese el segundo número (mayor que 0 y menor que 259): "))
+    if valor < 0 or valor > 259:
+        print("Error: el valor ingresado no cumple con las condiciones.")
+    else:
+        n2 = valor
+
+while n3 is None:
+    valor = int(input("Ingrese el tercer número (mayor que 0 y menor que 259): "))
+    if valor < 0 or valor > 259:
+        print("Error: el valor ingresado no cumple con las condiciones.")
+    else:
+        n3 = valor
+
+promedio = (n1 + n2 + n3) / 3
+
+print("El promedio es:", round(promedio, 2))
+
+numero_actual = promedio
+secuencia = str(numero_actual)
+
+while numero_actual != 1:
+    if numero_actual % 2 == 0:
+        numero_actual = numero_actual / 2
+    else:
+        numero_actual = (numero_actual * 3) + 1
+    
+    secuencia += " -> " + str(numero_actual)
+
+print("La secuencia de Collatz para el promedio es:", secuencia)
